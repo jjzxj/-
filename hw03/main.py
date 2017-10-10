22faa302454dd7c173d9798ac68d1a60da422da9
jjzxj <jjzxj@users.noreply.github.com>
2017-10-10 13:08:00 +0800

    Update main.py

--- hw03/main.py
+++ hw03/main.py
@@ -2,94 +2,35 @@ import turtle as t
 
 t.hideturtle()
 
-t.penup()
-
-t.goto(-576,384)
-
-t.pendown()
-
-t.color('red','red')
-
-t.begin_fill()
-for i in range(1):
-    t.forward(1152)
-    t.right(90)
-    t.forward(768)
-    t.right(90)
-    t.forward(1152)
-    t.right(90)
-    t.forward(768)
-t.end_fill()
-
-t.penup()
-#第一颗星
-t.goto(-384,307.2)
-
-t.pendown()
-
-t.right(162)
-t.color('yellow','yellow')
-t.begin_fill()
-for i in range(5):
-    t.forward(228)
-    t.right(144)
-t.end_fill()
-#第二颗星
-t.penup()
-
-t.goto(-230.4,290)
-
-t.pendown()
-
-t.left(120)
-
-t.begin_fill()
-for i in range(5):
-    t.forward(76)
-    t.right(144)
-t.end_fill()
-#第三颗星
-t.penup()
-
-t.goto(-150,220)
-
-t.pendown()
-
-t.right(20)
-
-t.begin_fill()
-for i in range(5):
-    t.forward(76)
-    t.right(144)
-
-t.end_fill()
-#第四颗星
-t.penup()
-
-t.goto(-150,120)
-
-t.pendown()
-
-t.right(25)
-
-t.begin_fill()
-for i in range(5):
-    t.forward(76)
-    t.right(144)
-
-t.end_fill()
-#第五颗星
-t.penup()
-
-t.goto(-230,70)
-
-t.pendown()
-
-t.right(25)
-
-t.begin_fill()
-for i in range(5):
-    t.forward(76)
-    t.right(144)
-
-t.end_fill()
+def start(x,y,right,f):
+    t.penup()
+    t.goto(x,y)
+    t.pendown()
+    t.right(right)
+    t.color('yellow','yellow')
+    t.begin_fill()
+    for i in range (5):
+        t.forward(f)
+        t.right(144)
+    t.end_fill()
+def rectangle(x,y,c,k):
+    t.penup()
+    t.goto(-575,383)
+    t.pendown()
+    t.color('red','red')
+    t.begin_fill()
+    for i in range(1):
+        t.forward(c)
+        t.right(90)
+        t.forward(k)
+        t.right(90)
+        t.forward(c)
+        t.right(90)
+        t.forward(k)
+    t.end_fill()
+rectangle(-575,383,1151,767)
+start(-384,307.2,162,228)
+start(-230.4,290,240,76)
+start(-150,220,20,76)
+start(-150,120,25,76)
+start(-230,70,25,76)
